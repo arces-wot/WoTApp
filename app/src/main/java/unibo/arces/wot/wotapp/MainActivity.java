568bcb150c4b431b4f2f6aab368d126138f31a94
Francesco Antoniazzi <francesco.antoniazzi1991@gmail.com>
2017-11-22 17:22:54 +0100

    working on QRcode read

--- app/src/main/java/unibo/arces/wot/wotapp/MainActivity.java
+++ app/src/main/java/unibo/arces/wot/wotapp/MainActivity.java
@@ -1,15 +1,34 @@
 package unibo.arces.wot.wotapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.SparseArray;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class MainActivity extends AppCompatActivity {
+    private final static int PHOTO_REQUEST = 10;
+    private BarcodeDetector qrDetector;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +37,16 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        qrDetector = new BarcodeDetector.Builder(getApplicationContext()).setBarcodeFormats(Barcode.QR_CODE).build();
+        if (!qrDetector.isOperational()) {
+            Snackbar.make(getCurrentFocus(), R.string.qrDetectorFailure, Snackbar.LENGTH_LONG).show();
+        }
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                takeQRPicture();
             }
         });
     }
@@ -48,5 +71,55 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void takeQRPicture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File photo = new File(Environment.getExternalStorageDirectory(), "picture.jpg");
+        imageUri = FileProvider.getUriForFile(getApplicationContext(),BuildConfig.APPLICATION_ID + ".fileprovider", photo);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, PHOTO_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ((requestCode==PHOTO_REQUEST) && (resultCode==RESULT_OK)) {
+            Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            mediaScanIntent.setData(imageUri);
+            this.sendBroadcast(mediaScanIntent);
+            try {
+                Bitmap bitmap = decodeBitmapUri(this, imageUri);
+                if (bitmap!=null) {
+                    Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                    SparseArray<Barcode> barcodes = qrDetector.detect(frame);
+                    if (barcodes.size()>0) {
+                        for (int index = 0; index < barcodes.size(); index++) {
+                            Barcode code = barcodes.valueAt(index);
+                            Snackbar.make(getCurrentFocus(), "Got value: " + code.displayValue, Snackbar.LENGTH_LONG).show();
+                        }
+                    }
+                    else Snackbar.make(getCurrentFocus(), R.string.scanned_nothing, Snackbar.LENGTH_LONG).show();
+                }
+            } catch (FileNotFoundException fne) {
+                Snackbar.make(getCurrentFocus(), R.string.failed_loading_image, Snackbar.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private Bitmap decodeBitmapUri(Context ctx, Uri uri) throws FileNotFoundException {
+        int targetW = 600;
+        int targetH = 600;
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(ctx.getContentResolver().openInputStream(uri), null, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+
+        return BitmapFactory.decodeStream(ctx.getContentResolver().openInputStream(uri), null, bmOptions);
     }
 }
